a30fda77f74fc7a7f0b488df0a027eb833b48c43
TyMassin <89612631+TyMassin@users.noreply.github.com>
2021-09-30 21:20:10 -0400

    Player prefab and basic game object created.
    This should be a good base to start playing around in. You can change this up and mess it up however you want to figure things out.

---
+++ U2D/Assets/ExampleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+
+    public bool checkbox; //I'm a public variable! I'm visible in the inspector!
+    public string textbox;
+
+    private bool nonEditableCheckbox; //I'm a private variable. I'm only editable through script and not the inspector.
+    private string nonEditableTextbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkbox = true; //When the object is first spawned. Checkbox is checked.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

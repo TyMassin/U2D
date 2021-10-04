15610d5919c77d0ddaa5bcdf09f1d80dafb46f04
N0dea <midukes2000@gmail.com>
2021-10-03 22:45:15 -0400

    left/right movement + etc
    -added some movement,
    -audio track plays when game start
    -2 test scripts
    -

---
+++ U2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement: MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 pos = transform.position;
+
+        pos.x += h *speed* Time.deltaTime;
+        pos.y += v * speed*Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
---
+++ U2D/Assets/Scripts/learningCsharp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class learningCsharp : MonoBehaviour
+{
+    private void Start()
+    {
+        int a = 10;
+        int b = 5;
+        int c = a + b;
+
+     
+        Debug.Log("A + B = "+c);
+    }
+}

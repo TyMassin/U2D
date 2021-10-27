95c244c50bf8b61c95527811724c5eaf9e6684c6
N0dea <midukes2000@gmail.com>
2021-10-27 14:17:30 -0400

    Added asset pack + disabled up and down movements

--- U2D/Assets/Scripts/PlayerMovement.cs
+++ U2D/Assets/Scripts/PlayerMovement.cs
@@ -15,13 +15,10 @@ public class PlayerMovement: MonoBehaviour
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
         Vector2 pos = transform.position;
-
-        pos.x += h *speed* Time.deltaTime;
-        pos.y += v * speed*Time.deltaTime;
-
+        pos.x += h * speed* Time.deltaTime;
         transform.position = pos;
     }
+
+    
 }

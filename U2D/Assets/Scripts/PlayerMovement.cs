167fceefc09660e42e49b1508304214f390139fb
N0dea <midukes2000@gmail.com>
2021-11-03 09:13:51 -0400

    Jump fn + main theme + basic environment
    -added relatively normal jump
    -added main theme for the game, a basic chiptune song
    -added simple background and grass to stand on, with dirt underneath.

--- U2D/Assets/Scripts/PlayerMovement.cs
+++ U2D/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement: MonoBehaviour
 {
+
     public float speed = 5;
     // Start is called before the first frame update
+
+    [SerializeField]
+    //Jump vars
+    private Rigidbody2D body;
+
+    public float jumpForce = 10f;
+    bool isGrounded; //Are we touching the ground?
+    private string GROUND_TAG = "Ground";
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
@@ -14,11 +28,37 @@ public class PlayerMovement: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        Vector2 pos = transform.position;
-        pos.x += h * speed* Time.deltaTime;
-        transform.position = pos;
+        MovePlayer();
+        PlayerJump();
     }
 
-    
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        Vector2 pos = transform.position;
+        pos.x += moveX * speed * Time.deltaTime;
+        transform.position = pos;
+
+
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            body.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG)) {
+            isGrounded = true;
+        }
+
+    }
+  
+
 }
